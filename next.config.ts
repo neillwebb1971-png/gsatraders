32a4bc905abac86535823dc04882e916d1d3c919
neillwebb1971-png <neillwebb1971@gmail.com>
2026-09-18 13:19:11 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
   turbopack: {
     root: path.resolve(__dirname),
   },
